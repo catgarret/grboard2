7eb8659fab0d5e24db1392daaccae17ac9284412
Lee Dong-Kyu (Guleum) <official@guleum.me>
2018-03-14 21:55:06 +0900

    nav align center

--- module/board/admin/skin/basic/index.php
+++ module/board/admin/skin/basic/index.php
@@ -23,43 +23,45 @@
 	<body>
 	
 	<nav class="navbar navbar-expand-md navbar-dark bg-dark" role="navigation">
-		<a class="navbar-brand" href="#">Administrator's Page</a>
-			
-		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#grboard2TopNav" aria-controls="grboard2TopNav" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggle-icon"></span>
-		</button>		
-		
-		<div id="grboard2TopNav" class="navbar-collapse collapse">
-			<ul class="nav navbar-nav mr-auto">
-				<li class="nav-item active"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>" data-toggle="tooltip" title="사이트 첫 화면으로 이동 합니다" class="nav-link">
-					Home</a></li>
-				<li class="nav-item dropdown">
-					<a href="#" class="nav-link dropdown-toggle" role="button" id="navBBSMenu" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
-					게시판 관리</a>
-					<div class="dropdown-menu" aria-labelledby="navBBSMenu">
-						<a class="dropdown-item" href="/<?php echo $grboard; ?>/board/admin/board/add" 
-							data-toggle="tooltip" data-placement="right" title="<?php echo $lang['board_add_info']; ?>">게시판 추가하기</a>
-						<a class="dropdown-item" href="/<?php echo $grboard; ?>/board/admin/board/list"
-							data-toggle="tooltip" data-placement="right" title="<?php echo $lang['board_list_info']; ?>">게시판 목록보기</a>
-						<a class="dropdown-item" href="/<?php echo $grboard; ?>/board/admin/board/group"
-							data-toggle="tooltip" data-placement="right" title="<?php echo $lang['board_group_info']; ?>">게시판 그룹보기</a>
-					</div>
-				</li>
-				<li class="nav-item dropdown">
-					<a href="#" class="nav-link dropdown-toggle" role="button" id="navMemberMenu" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
-					회원 관리</a>
-					<div class="dropdown-menu" aria-labelledby="navMemberMenu">
-						<a class="dropdown-item" href="/<?php echo $grboard; ?>/board/admin/member/add" 
-							data-toggle="tooltip" data-placement="right" title="<?php echo $lang['member_add_info']; ?>">회원 직접 추가하기</a>
-						<a class="dropdown-item" href="/<?php echo $grboard; ?>/board/admin/member/list"
-							data-toggle="tooltip" data-placement="right" title="<?php echo $lang['member_list_info']; ?>">회원 목록보기</a>
-						<a class="dropdown-item" href="/<?php echo $grboard; ?>/board/admin/member/group"
-							data-toggle="tooltip" data-placement="right" title="<?php echo $lang['member_group_info']; ?>">회원 그룹보기</a>
-					</div>
-				</li>
-				<li class="nav-item"><a href="http://sirini.net" data-toggle="tooltip" title="Powered by GR Board 2 in GRBOARD.com" class="nav-link"> 
-					GR Board <sup>2</sup></a></li>
-			</ul>
+		<div class="container">
+			<a class="navbar-brand" href="#">Administrator's Page</a>
+
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#grboard2TopNav" aria-controls="grboard2TopNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggle-icon"></span>
+			</button>		
+
+			<div id="grboard2TopNav" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav mr-auto">
+					<li class="nav-item active"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>" data-toggle="tooltip" title="사이트 첫 화면으로 이동 합니다" class="nav-link">
+						Home</a></li>
+					<li class="nav-item dropdown">
+						<a href="#" class="nav-link dropdown-toggle" role="button" id="navBBSMenu" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
+						게시판 관리</a>
+						<div class="dropdown-menu" aria-labelledby="navBBSMenu">
+							<a class="dropdown-item" href="/<?php echo $grboard; ?>/board/admin/board/add" 
+								data-toggle="tooltip" data-placement="right" title="<?php echo $lang['board_add_info']; ?>">게시판 추가하기</a>
+							<a class="dropdown-item" href="/<?php echo $grboard; ?>/board/admin/board/list"
+								data-toggle="tooltip" data-placement="right" title="<?php echo $lang['board_list_info']; ?>">게시판 목록보기</a>
+							<a class="dropdown-item" href="/<?php echo $grboard; ?>/board/admin/board/group"
+								data-toggle="tooltip" data-placement="right" title="<?php echo $lang['board_group_info']; ?>">게시판 그룹보기</a>
+						</div>
+					</li>
+					<li class="nav-item dropdown">
+						<a href="#" class="nav-link dropdown-toggle" role="button" id="navMemberMenu" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
+						회원 관리</a>
+						<div class="dropdown-menu" aria-labelledby="navMemberMenu">
+							<a class="dropdown-item" href="/<?php echo $grboard; ?>/board/admin/member/add" 
+								data-toggle="tooltip" data-placement="right" title="<?php echo $lang['member_add_info']; ?>">회원 직접 추가하기</a>
+							<a class="dropdown-item" href="/<?php echo $grboard; ?>/board/admin/member/list"
+								data-toggle="tooltip" data-placement="right" title="<?php echo $lang['member_list_info']; ?>">회원 목록보기</a>
+							<a class="dropdown-item" href="/<?php echo $grboard; ?>/board/admin/member/group"
+								data-toggle="tooltip" data-placement="right" title="<?php echo $lang['member_group_info']; ?>">회원 그룹보기</a>
+						</div>
+					</li>
+					<li class="nav-item"><a href="http://sirini.net" data-toggle="tooltip" title="Powered by GR Board 2 in GRBOARD.com" class="nav-link"> 
+						GR Board <sup>2</sup></a></li>
+				</ul>
+			</div>
 		</div>
 	</nav>			
 	
