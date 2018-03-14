baf8484311649808c7ba74747d1541ebfb9cf082
Lee Dong-Kyu (Guleum) <official@guleum.me>
2018-03-14 21:56:37 +0900

    navi align center

--- module/board/login/skin/basic/index.php
+++ module/board/login/skin/basic/index.php
@@ -9,8 +9,8 @@
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
 		<link href="<?php echo $skinResourcePath; ?>/login.css" rel="stylesheet" media="screen" />
 
-	    <script src="/<?php echo $grboard; ?>/lib/jquery.js"></script>
-    	<script src="/<?php echo $grboard; ?>/lib/popper.js"></script>
+	    	<script src="/<?php echo $grboard; ?>/lib/jquery.js"></script>
+    		<script src="/<?php echo $grboard; ?>/lib/popper.js"></script>
 		<script src="/<?php echo $grboard; ?>/lib/bootstrap_material_design/js/bootstrap-material-design.min.js"></script>		
 		<script>
 		$(function(){
@@ -22,23 +22,25 @@
 	<body>
 	
 	<nav class="navbar navbar-expand-md navbar-dark bg-dark" role="navigation">
-		<a class="navbar-brand" href="#"><?php echo $_SERVER['HTTP_HOST']; ?></a>
-			
-		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#grboard2TopNav" aria-controls="grboard2TopNav" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggle-icon"></span>
-		</button>		
-		
-		<div id="grboard2TopNav" class="navbar-collapse collapse">
-			<ul class="nav navbar-nav mr-auto">
-				<li class="nav-item active"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>" data-toggle="tooltip" title="<?php echo $lang['home_info']; ?>" class="nav-link">
-					Home <span class="sr-only">(current)</span></a></li>
-				<li class="nav-item"><a href="<?php echo $moveBackPath; ?>" data-toggle="tooltip" title="이전 화면으로 돌아갑니다" class="nav-link"> 
-					Back</a></li>	
-				<li class="nav-item"><a href="<?php echo $prePath; ?>/join/0" data-toggle="tooltip" title="계정이 없을 경우 여기를 클릭하여 회원 가입을 하실 수 있습니다" class="nav-link"> 
-					Join</a></li>	
-				<li class="nav-item"><a href="http://sirini.net" data-toggle="tooltip" title="Powered by GR Board 2 in GRBOARD.com" class="nav-link"> 
-					GR Board <sup>2</sup></a></li>
-			</ul>
+		<div class="container">
+			<a class="navbar-brand" href="#"><?php echo $_SERVER['HTTP_HOST']; ?></a>
+
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#grboard2TopNav" aria-controls="grboard2TopNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggle-icon"></span>
+			</button>		
+
+			<div id="grboard2TopNav" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav mr-auto">
+					<li class="nav-item active"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>" data-toggle="tooltip" title="<?php echo $lang['home_info']; ?>" class="nav-link">
+						Home <span class="sr-only">(current)</span></a></li>
+					<li class="nav-item"><a href="<?php echo $moveBackPath; ?>" data-toggle="tooltip" title="이전 화면으로 돌아갑니다" class="nav-link"> 
+						Back</a></li>	
+					<li class="nav-item"><a href="<?php echo $prePath; ?>/join/0" data-toggle="tooltip" title="계정이 없을 경우 여기를 클릭하여 회원 가입을 하실 수 있습니다" class="nav-link"> 
+						Join</a></li>	
+					<li class="nav-item"><a href="http://sirini.net" data-toggle="tooltip" title="Powered by GR Board 2 in GRBOARD.com" class="nav-link"> 
+						GR Board <sup>2</sup></a></li>
+				</ul>
+			</div>
 		</div>
 	</nav>	
 		
